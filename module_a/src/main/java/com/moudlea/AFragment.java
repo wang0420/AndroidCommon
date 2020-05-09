435d08437eab0d99af0ee96b7d1b42492accb1a1
王伟 <wei.wang23@zhenai.com>
2020-05-09 17:31:24 +0800

    leetcode

--- module_a/src/main/java/com/moudlea/AFragment.java
+++ module_a/src/main/java/com/moudlea/AFragment.java
@@ -1,6 +1,9 @@
 package com.moudlea;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +18,7 @@ import com.basemodule.service.IUserModuleService;
 import com.moudlea.jetpack.JetPackStudyActivity;
 import com.moudlea.rxjava.RxActivity;
 
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import butterknife.BindView;
 import butterknife.OnClick;
 
@@ -47,9 +51,33 @@ public class AFragment extends BaseFragment {
     @Override
     protected void initView(View view) {
         setListener();
+
+
+        LocalBroadcastManager broadcastManager = LocalBroadcastManager.getInstance(mActivity);
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("ww.LocalBroadcastManager");
+        broadcastManager.registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String change = intent.getStringExtra("change");
+                Toast.makeText(mActivity, change, Toast.LENGTH_LONG).show();
+            }
+        }, intentFilter);
+
     }
 
-    @OnClick({R2.id.btn4, R2.id.btn5, R2.id.btn6})
+
+    /**
+     * 注销广播
+     */
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        //broadcastManager.unregisterReceiver(mAdDownLoadReceiver);
+    }
+
+
+    @OnClick({R2.id.btn4, R2.id.btn5, R2.id.btn6, R2.id.btn7})
     void onViewClicked(View view) {
         int id = view.getId();
         if (id == R.id.btn4) {
@@ -58,6 +86,13 @@ public class AFragment extends BaseFragment {
             startActivity(new Intent(mActivity, JetPackStudyActivity.class));
         } else if (id == R.id.btn6) {
             startActivity(new Intent(mActivity, RxActivity.class));
+        } else if (id == R.id.btn7) {
+            //https://www.cnblogs.com/zhaoyanjun/p/6048369.html
+            //发送广播， LocalBroadcastManager用的单例模式，getInstance获取对象即可调用发送 注册等；使用完成也要取消注册
+            Intent intent = new Intent("ww.LocalBroadcastManager");
+            intent.putExtra("change", "yes");
+            LocalBroadcastManager.getInstance(mActivity).sendBroadcast(intent);
+
         }
     }
 
---
+++ module_base/src/main/java/com/basemodule/utils/LogUtil.java
@@ -0,0 +1,37 @@
+package com.basemodule.utils;
+
+import android.util.Log;
+
+/**
+ * Created by wangwei on 2020/5/9.
+ * Android系统对日志长度有限制的，最大长度为4K（注意是字符串的长度），超过这个范围的自动截断，所以就会出现打印不全的情况。
+ * 从Android内核源码来看，在logger.h头文件中有以下宏定义：
+ */
+
+public class LogUtil {
+
+    private static int LOG_MAX_LENGTH = 4000;
+    private static String TAG = "TAG";
+
+    public static void d(String msg) {
+        w(TAG, msg);
+    }
+
+    public static void w(String msg) {
+        w(TAG, msg);
+    }
+
+    private static void w(String TAG, String msg) {
+        int strLength = msg.length();
+        int start = 0;
+        int end = LOG_MAX_LENGTH;
+        while (end < strLength) {
+            Log.w(TAG, msg.substring(start, end));
+            start = end;
+            end = end + LOG_MAX_LENGTH;
+        }
+        Log.w(TAG, msg.substring(start, strLength));
+    }
+
+
+}
