919b5dad7c3bad3403f080cf1fe1d004233cc9e0
wangwei <709693972@qq.com>
2020-06-19 16:09:13 +0800

    module名称

--- module_base/src/main/java/com/android/common/net/UpLoadCallBack.java
+++
@@ -1,9 +0,0 @@
-package com.android.common.net;
-
-
-/**
- * 文件上传回调
- */
-public interface UpLoadCallBack {
-    void success(long progress);
-}
--- module_base/src/main/java/com/android/common/net/UploadProgressListener.java
+++
@@ -1,9 +0,0 @@
-package com.android.common.net;
-
-/**
- * Created by popper on 2017/11/24.
- */
-
-public interface UploadProgressListener {
-    void onProgress(long currentCount, long totalCount);
-}
